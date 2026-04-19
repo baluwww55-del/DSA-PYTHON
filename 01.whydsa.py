1bffe61a44001e7d9b953be84ad03c8f960e566b
Balu <balu.www.55@gmail.com>
2026-04-19 10:50:44 +0530

    Add files via upload

---
+++ 01.whydsa.py
@@ -0,0 +1,39 @@
+# DSA = data structures and algorithms . 
+# DSA makes us better software developers like who solve the real world problems in an efficiently. 
+# DSA gets a job done in less time and with less resources.
+
+# lets dive and jump into the world of DSA.
+
+# 1. What is dsa ?
+# dsa stands for data structures and algorithms.
+
+# First concept of dsa is :
+# order of growth : it is the way to measure the efficiency of an algorithm.
+# it i denoted by the big o notation. 
+# it is used to describe the upper bound of the time complexity of an algorithm.
+
+# there is an sequence of growth rates for different algorithms.
+# 1<logn<n<nlogn<n^2<2^n<n! 
+# this is the sequence of growth rates for different algorithms.
+
+# if an alogorithm  has a time complexity of O(1) then it is called constant time algorithm and known as good algorithm.
+# if an algorithm has a time complexity of O(n) then it is called linear time algorithm and known as bad algorithm.
+# if an algorithm has a time complexity ofO(n^2) then it is called quadratic time algorithm and known as worst algorithm.
+# usualy goood and bad algorithms depends on the input size.
+
+# time complexity of big O is = f(n) and g(n) >= c*f(n) for all n >= n0 where c is a constant and n0 is a positive integer.)
+# 
+#  OMEGA NOTATION : It is used to describe the lower bound of the time complexity of an algorithm.
+# OMEGA NOTATION : It is used to describe the lower bound of the time complexity of an algorithm.
+# it is also known as the best case of time complexity of an algorithm.
+# # OMEGA NOTATION : F(n) and g(n) <= c*f(n) for all n >= n0 where c is a constant and n0 is a positive integer.)
+# 
+# THETA NOTATION : It is used to describe the tight bound of the time complexity of an algorithm.
+# it is also known as the average case of time complexity of an algorithm.
+# THETA NOTATION : F(n) and g(n) <= c1*f(n) and g(n) >= c2*f(n) for all n >= n0 where c1 and c2 are constants and n0 is a positive integer.)
+# # SPACE COMPLEXITY : It is used to describe the amount of memory space required by an algorithm to run.
+# SPACE COMPLEXITY : It is used to describe the amount of memory space required by an algorithm to run.
+# it is denoted by the big O notation.
+# SPACE COMPLEXITY : It is used to describe the amount of memory space required by an
+#  algorithm to run. It is denoted by the big O notation. It is used to describe the upper bound of the space complexity of an algorithm.
+# SPACE COMPLEXITY : It is used to describe the amount of memory space required by an algorithm to run. It is denoted by the big O notation. It is used to describe the upper bound of the space complexity of an algorithm. It is also known as the worst case of space complexity of an algorithm. 
