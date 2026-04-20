6dde665f4a470e1f824946b9901ff5f2b22f81a2
Balu <balu.www.55@gmail.com>
2026-04-21 00:20:09 +0530

    Add files via upload

---
+++ intro-dsa/03.bit_magic_operators.py
@@ -0,0 +1,24 @@
+# bit magic operators in python part_1 :
+# decimal to binary :
+print(bin(18)) # 0b10010 is the binary representation of 18.
+
+# bitwise and operator (&) :
+x = 3
+y = 6
+print(x & y)
+
+# bitwise or operator (+) :
+x = 4 
+y = 10 
+print(x/y)
+
+# bitwise x-or operator :
+x = 3 
+y = 6 
+print(x^y)
+
+# bitwise not operator :        
+x = 3
+print(~x)
+# it always the toggles the given inputs not operator / inverted operator in python .
+
